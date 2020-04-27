8fb81b4ba70973129f016e857bfb1d7fef89cc8c
yexiaoqi <angel7ge7@163.com>
2020-04-27 10:36:18 +0800

    mnist分类训练例子

--- learn.py
+++ learn.py
@@ -37,29 +37,30 @@ def add_layer(inputs,in_size,out_size,n_layer,activation_function=None):
         tf.summary.histogram(layer_name+'/outputs',outputs)
     return outputs
 
-def compute_accuracy(v_xs,v_ys):
+
+def compute_accuracy(x_vs,y_vs):
     global prediction
-    y_pre=sess.run(prediction,feed_dict={xs:v_xs})
-    correct_prediction=tf.equal(tf.argmax(y_pre,1),tf.argmax(v_ys,1))
+    y_pre=sess.run(prediction,feed_dict={xs:x_vs})
+    correct_prediction=tf.equal(tf.argmax(y_pre,1),tf.argmax(y_vs,1))
     accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-    result=sess.run(accuracy,feed_dict={xs:v_xs,ys:v_ys})
+    result=sess.run(accuracy,feed_dict={xs:x_vs,ys:y_vs})
     return result
 
+mnist=input_data.read_data_sets('MNIST_data/',one_hot=True)
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+with tf.name_scope('inputs'):
+    xs=tf.placeholder(tf.float32,[None,28*28],name='x_input')
+    ys=tf.placeholder(tf.float32,[None,10],name='y_input')
 
-xs=tf.placeholder(tf.float32,[None,28*28])
-ys=tf.placeholder(tf.float32,[None,10])
 prediction=add_layer(xs,28*28,10,n_layer=1,activation_function=tf.nn.softmax)
 cross_entropy=tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
 train_step=tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-sess=tf.Session()
 init=tf.global_variables_initializer()
+sess=tf.Session()
 sess.run(init)
 
-
 for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys})
+    batch_xs,batch_ys=mnist.train.next_batch(100)
+    sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
     if i%50==0:
         print(compute_accuracy(mnist.test.images,mnist.test.labels))
