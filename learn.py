1e296c981f14388be144b2b9d75950075be62984
yexiaoqi <angel7ge7@163.com>
2020-04-28 12:20:05 +0800

    saver保存读取

--- learn.py
+++ learn.py
@@ -14,65 +14,10 @@ import matplotlib.pyplot as plt
 #from tensorflow.examples.tutorials.mnist import input_data
 import input_data
 
-mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
-
-def weight_variable(shape):
-    initial=tf.truncated_normal(shape,stddev=0.1)
-    return tf.Variable(initial)
-
-def bias_variable(shape):
-    initial=tf.constant(0.1,shape=shape)
-    return tf.Variable(initial)
-
-def conv2d(x,w):
-    return tf.nn.conv2d(x,w,strides=[1,1,1,1],padding='SAME')
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding='SAME')
-
-def compute_accuracy(x_vs,y_vs):
-    global prediction
-    y_pre=sess.run(prediction,feed_dict={xs:x_vs,ys:y_vs,keep_prob:1})
-    correct_prediction=tf.equal(tf.argmax(y_pre,1),tf.argmax(y_vs,1))
-    accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-    result=sess.run(accuracy,feed_dict={xs:x_vs,ys:y_vs,keep_prob:1})
-    return result
-
-xs=tf.placeholder(tf.float32,[None,28*28])/255.
-ys=tf.placeholder(tf.float32,[None,10])
-keep_prob=tf.placeholder(tf.float32)
-x_image=tf.reshape(xs,[-1,28,28,1])
-
-w_conv1=weight_variable([5,5,1,32])
-b_conv1=bias_variable([32])
-h_conv1=tf.nn.relu(conv2d(x_image,w_conv1)+b_conv1)
-h_pool1=max_pool_2x2(h_conv1)
-
-w_conv2=weight_variable([5,5,32,64])
-b_conv2=bias_variable([64])
-h_conv2=tf.nn.relu(conv2d(h_pool1,w_conv2)+b_conv2)
-h_pool2=max_pool_2x2(h_conv2)
-h_pool2_flat=tf.reshape(h_pool2,[-1,7*7*64])
-
-w_fc1=weight_variable([7*7*64,1024])
-b_fc1=bias_variable([1024])
-h_fc1=tf.nn.relu(tf.matmul(h_pool2_flat,w_fc1)+b_fc1)
-h_fc1_drop=tf.nn.dropout(h_fc1,keep_prob)
-
-w_fc2=weight_variable([1024,10])
-b_fc2=bias_variable([10])
-prediction=tf.nn.softmax(tf.matmul(h_fc1_drop,w_fc2)+b_fc2)
-
-cross_entropy=tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),reduction_indices=[1]))
-train_step=tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-
-init=tf.global_variables_initializer()
-sess=tf.Session()
-sess.run(init)
-
-for i in range(1000):
-    batch_xs,batch_ys=mnist.train.next_batch(100)
-    sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys,keep_prob:0.5})
-    if i%50==0:
-        print(compute_accuracy(mnist.test.images[:1000],mnist.test.labels[:1000]))
-
+w=tf.Variable(np.arange(6).reshape(2,3),dtype=tf.float32,name='weights')
+b=tf.Variable(np.arange(3).reshape(1,3),dtype=tf.float32,name='biases')
+saver=tf.train.Saver()
+with tf.Session() as sess:
+    saver.restore(sess,"200428/trainsave.ckpt")
+    print('weights',sess.run(w))
+    print('biases',sess.run(b))
---
+++ saver.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+if __name__=='__main__':
+    #保存
+    w=tf.Variable([[1,2,3],[3,4,5]],dtype=tf.float32,name='weights')
+    b=tf.Variable([[1,2,3]],dtype=tf.float32,name='biases')
+
+    init=tf.global_variables_initializer()
+    saver=tf.train.Saver()
+    with tf.Session() as sess:
+        sess.run(init)
+        save_path=saver.save(sess,'net200427/200427net.ckpt')
+        print("save to path:",save_path)
+
+
+    #提取
+    w=tf.Variable(np.arange(6).reshape(2,3),dtype=tf.float32,name='weights')
+    b=tf.Variable(np.arange(3).reshape(1,3),dtype=tf.float32,name='biases')
+
+    saver=tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess,'net200427/200427net.ckpt')
+        print('weights:',sess.run(w))
+        print('biases',sess.run(b))
