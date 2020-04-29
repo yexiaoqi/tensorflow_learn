cbd8ddd7f517679fc93759d9260b67f00e1c1a6f
yexiaoqi <angel7ge7@163.com>
2020-04-29 18:31:37 +0800

    设置坐标轴

--- learn_matplotlib/basic_application.py
+++ learn_matplotlib/basic_application.py
@@ -13,6 +13,17 @@ plt.plot(x,y1)
 # plt.plot(x,y2,c='red',lw=1.0,ls='--')
 plt.plot(x,y2,color='red',linewidth=1.0,linestyle='--')#这个和上面等价
 
+plt.xlim((-1,2))
+plt.ylim((-2,3))
+plt.xlabel('I am x')
+plt.ylabel('I am y')
+
+new_ticks=np.linspace(-1,2,5)
+print(new_ticks)
+plt.xticks(new_ticks)
+
+# 格式化转义 字符串首尾 r'$...$' （matplotlib中），就是把\ 变成了空格，实际上直接'really bad'这种字符串也行
+plt.yticks([-2,-1.8,-1,1.22,3],['really bad',r'$bad$',r'$normal$',r'$good$',r'$really\ good$'])
 
 plt.show()
 
