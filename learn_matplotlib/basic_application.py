72d71e25d091cd0250ca8d3ad785b2742c55a0f0
yexiaoqi <angel7ge7@163.com>
2020-04-29 18:15:24 +0800

    figure图像

--- learn_matplotlib/basic_application.py
+++ learn_matplotlib/basic_application.py
@@ -2,7 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.linspace(-1,1,300)
-y=np.square(x)
+y1=np.square(x)
+y2=2*x+1
+
 plt.figure()
-plt.plot(x,y)
+plt.plot(x,y1)
+
+plt.figure(num=3,figsize=(8,3))
+plt.plot(x,y1)
+# plt.plot(x,y2,c='red',lw=1.0,ls='--')
+plt.plot(x,y2,color='red',linewidth=1.0,linestyle='--')#这个和上面等价
+
+
 plt.show()
+
